5e7424d25d506b885630aafc875586f6334560dc
Sharmina Akhtar <sakht318@gmail.com>
2018-03-05 13:09:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+      "Hello, World!"
+    end
+
+    get '/name' do
+      "My name is Mina!"
+    end
+
+    get '/hometown' do
+      "My hometown is NYC!"
+    end
+
+    get '/favorite-song' do
+      "My favorite song is 'Hello' by Adele!"
+    end
+
 end
